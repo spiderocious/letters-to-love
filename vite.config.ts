0412421603d40b9390bd6ae399e268e54df4f48c
Oluwaferanmi Adeniji <feranmidev@gmail.com>
2026-02-25 08:48:36 +0100

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@app': path.resolve(__dirname, 'src'),
+      '@features': path.resolve(__dirname, 'src/features'),
+      '@shared': path.resolve(__dirname, 'src/shared'),
+      '@ui': path.resolve(__dirname, 'src/ui'),
+    },
+  },
+})
